b1797e3d36575b525ff86470ea880f2351741c38
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-05-14 21:43:49 -0300

    Alterado forma de filter para todas as páginas em uma única classe

--- src/br/com/fiap/filter/FilterLogin.java
+++ src/br/com/fiap/filter/FilterLogin.java
@@ -5,23 +5,20 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import br.com.fiap.model.Usuario;
-
-@WebFilter("/usuario/*")
-public class FilterUsuario implements Filter {
-
-	public FilterUsuario() {
+@WebFilter("/*")
+public class FilterLogin implements Filter{
+	
+	public FilterLogin() {
 		super();
 	}
-	
+
 	@Override
 	public void destroy() {
 		
@@ -30,15 +27,15 @@ public class FilterUsuario implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-
-		HttpSession session = ((HttpServletRequest) request).getSession();
-		Usuario usuario = (Usuario) session.getAttribute("session_usuario");
 		
-		if(usuario == null) {
-			((HttpServletResponse) response).sendRedirect("/AmigoSecreto");
-		} else {
-			chain.doFilter(request, response);
+        HttpServletRequest req = (HttpServletRequest) request;
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/");
+		
+		if (req.getServletPath().equals("/")) {
+			dispatcher.forward(request, response);			
 		}
+		
+		chain.doFilter(request, response);
 	}
 
 	@Override
